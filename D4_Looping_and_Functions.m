e85ed6e9722e4e445eea88af027ec861a9a1d578
Rupak Kumar <physicsrupak@gmail.com>
2021-07-19 20:07:56 +0530

    Update D4_Looping_and_Functions.m

--- D4_Looping_and_Functions.m
+++ D4_Looping_and_Functions.m
@@ -5,3 +5,19 @@ if n>0
 else
     disp("Negative Number")
 end
+
+%%% Cheaking Leap Year or Non-Leap year %%%
+y = input("Enter year")
+if rem(y,4)==0
+    if rem(y,100)==0
+        if rem(y,400)==0
+            disp("Century Leap Year")
+        else
+            disp("Century Non-Leap Year")
+        end
+    else
+        disp("Leap Year")
+    end 
+else
+    disp("Not a Leap Year")
+end
