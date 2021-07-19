a707580e6bf68ec7ba47c0fdc0fc82ecac1eb19e
Rupak Kumar <physicsrupak@gmail.com>
2021-07-19 16:34:32 +0530

    Update D3_Assignment_01.m

--- D3_Assignment_01.m
+++ D3_Assignment_01.m
@@ -1,3 +1,6 @@
 % Make a random vector of 1x1000 size. Find out how many elements are between 0.4 and 0.6
 v = rand (1,1000);
 sum (0.4<v>0.6)
+
+%Make a vector from 0 to 2π with a step size of 0.01
+x = [0:0.01:2*pi]
