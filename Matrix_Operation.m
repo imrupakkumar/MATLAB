0c386b4ee456c002c8f7c22cdf82e5ce4c9703e6
Rupak Kumar <physicsrupak@gmail.com>
2021-07-19 14:01:46 +0530

    Add files via upload

---
+++ Matrix_Operation.m
@@ -0,0 +1,17 @@
+%%% Matrix Operation%%%
+% Addition, Sub & Multiplication %
+a = [1,2
+    3,4]
+b= [5,6
+    7,8]
+add = a+b
+Sub = a-b
+multily = a*b
+
+%%% element multiplication %%%
+m = a.*b %using (.)
+
+%element division
+d = a./b %(right dot division means a/b)
+d1 = a.\b %(left dot division means b/a)
+
