7c5779da080c152da6b795cd29094b4555fae9d5
Rupak Kumar <physicsrupak@gmail.com>
2021-07-29 00:35:49 +0530

    Update D7_Assignment_03.m

--- D7_Assignment_03.m
+++ D7_Assignment_03.m
@@ -6,3 +6,13 @@ function [df] = derv(p) % save the file with the name of derv.m in matlab
     d = d(1:end-1) 
 end
 
+% Write a function in MATLAB that takes a polynomial p as the input and returns its integration.
+function [If] = inti(p) % save the file with the name of inti.m in matlab
+    degree = length(p)-1;
+    m=degree:-1:0;
+    d= p./(m+1);
+    k=0;
+    d=[d k];
+    disp(d)
+end
+
