f2f2c0f7283a5cc86e19a9f1b62892f726a2e895
Rupak Kumar <physicsrupak@gmail.com>
2021-07-28 23:43:34 +0530

    Update D7_Assignment_03.m

--- D7_Assignment_03.m
+++ D7_Assignment_03.m
@@ -1,1 +1,8 @@
+% Write a function in MATLAB that takes a polynomial p as the input and returns its derivative.
+function [df] = derv(p) % save the file with the name of derv.m in matlab
+    degree = length(p)-1;
+    m=degree:-1:0;
+    d=p.*m;
+    d = d(1:end-1) 
+end
 
