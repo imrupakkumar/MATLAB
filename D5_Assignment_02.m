cebe25c2bec11d6c59a627ba8faf173af1a005c0
Rupak Kumar <physicsrupak@gmail.com>
2021-07-30 15:01:27 +0530

    Update D5_Assignment_02.m

--- D5_Assignment_02.m
+++ D5_Assignment_02.m
@@ -20,3 +20,27 @@ n=1:10;
 X = 1./(n.^2);
 s = sum(X);
 disp(s)
+
+% Write a program to generate factorial of a number And break the loop as soon as factorial cross 1000.
+% Print the current number for which factorial crosses 1000.
+
+for i=0:inf
+    f=1;
+    for j=i:-1:1
+        f=f*j;
+    end
+    if f>1000
+        fprintf("factorial of %d is %d",i,f)
+        break
+    else
+        fprintf("factorial of %d is %d\n",i,f)
+    end
+end
+
+
+% Make a function which takes four inputs a,b,m,n and give a (m x n) matrix
+% whose elements are random numbers between a and b
+
+function out = random_values(a,b,m,n) % Save the file with the name of (random_values.m)
+    out = a+(b-a)*rand(m,n);
+end
