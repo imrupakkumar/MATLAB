27ab54803b0f465bb155f1c84501af36a0d2ae84
Rupak Kumar <physicsrupak@gmail.com>
2021-08-10 17:29:48 +0530

    Update D6_graphs_funct.m

--- D6_graphs_funct.m
+++ D6_graphs_funct.m
@@ -3,4 +3,10 @@ clc;
 % Simple ploting b/w two co-ordinate
 x = [2,5,6,8,4];
 y = [4,7,2,1,4];
-plot(x,y)       % plot function is used for ploting in 2D
+plot(x,y)       % plot function is used for ploting in 2D. A graph will appear in right side of editor.
+
+% plot the graph of sin(x) between [0:2*pi]
+x = [0:0.1:2*pi];
+y = sin(x);
+plot(x,y)
+
